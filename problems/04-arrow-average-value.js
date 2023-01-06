f96f2dc1377f37499e74d89b5a03993eb11bc31c
Luis Castillo <luiscastillo@Luiss-MBP-5.attlocal.net>
2023-01-06 14:20:43 -0800

    5

--- problems/04-arrow-average-value.js
+++ problems/04-arrow-average-value.js
@@ -9,7 +9,14 @@ arrowAvgValue([10, 20]); // => 15
 arrowAvgValue([2, 3, 7]); // => 4
 arrowAvgValue([100, 60, 64]); // => 74.66666666666667
 ***********************************************************************/
-
+const  arrowAvgValue = (array) =>{
+  let sum = 0
+  for (let i =0 ; i<array.length; i++){
+    let current = array[i]
+    sum += current 
+  }
+  return sum/array.length
+}
 // Your code here
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
